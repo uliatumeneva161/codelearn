8ed909b82b3176c6257a27b6f3f20b2cfa88200f
julia <julia_gtyumenevaya_011022@mail.ru>
2026-04-26 12:48:53 +0300

    s.js - create curl -X PUT req

--- s.js
+++ s.js
@@ -1,21 +1,72 @@
 const http = require('http');
+const crypto = require('crypto');
 
 const port = 3000
+ let courses = []
 const server = http.createServer((req, res) => { 
-    if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-type': 'application/json' })
-       res.end('{"status":"starting"}');  
 
-    } else if(req.method === 'POST'){
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    const path = url.pathname
+    const method = req.method
+   
+    
+    console.log(path)
+    if (method === 'GET') {
+        res.writeHead(200, { 'Content-type': 'application/json' })
+        res.end('{"status":"starting"}');
+
+    } else if (method === 'POST') {
         let body = ''
+        req.on('data', (chunk) => {
+            body += chunk.toString()
+        })
+
+        req.on('end', () => {
+            res.writeHead(200, { 'Content-type': 'application/json' })
+            res.end(body);
+        })
+    } else if (method == 'PUT' && path === '/courses') { 
+       
+        let body = ""
+
         req.on('data', (chunk) => { 
             body += chunk.toString()
         })
 
         req.on('end', () => { 
-            res.writeHead(200, { 'Content-type': 'application/json' })
-             res.end(body);
+            let parsedBody
+
+            try { 
+                parsedBody = JSON.parse(body)
+            } catch(err) {
+                res.writeHead('400', { 'Content-Type': 'application/json' })
+                res.end(JSON.stringify({ error: 'Invalid JSON format' }));
+                return 
+            }
+
+             const {title, desc} = parsedBody
+        const newCourse = {
+            id: crypto.randomUUID(),
+            title: title,
+            desc: desc
+            }
+            
+        if (parsedBody.title === '' || parsedBody.desc === '') { 
+            res.writeHead(400, { 'Content-type': 'application/json'})
+            res.end(JSON.stringify({ 'err': 'empty folder!' }))
+            return 
+        }
+        courses.push(newCourse)
+            res.writeHead(201, { 'Content-type': 'application/json' })
+            
+            res.end(JSON.stringify(newCourse))
+            
+         console.log(`Added course: ${newCourse.id}`);
+        console.log(`Total courses: ${courses.length}`);
         })
+        
+       
+
     }
 })
 server.listen(port, () => { 
--- s2.js
+++ s2.js
@@ -1,21 +1,62 @@
 const http = require('http')
+const crypto = require('crypto');
+const courses = []
 
 const server = http.createServer((req, res) => { 
-    if (req.method === "GET") {
+
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const path = url.pathname;
+    console.log(path)
+    const method = req.method;
+
+    if (method === "GET") {
         res.writeHead(200, { "Content-type": "application/json" })
         res.end('{"sms" : "ok!"}')
     
-    } else if (req.method === "POST") {
-        const sms = ''
+    } else if (method === "POST") {
+        let sms = ''
 
-        req.on('data', (chunk) => { // 'data' ??
-            sms += chunk.toString()    
+        req.on('data', (chunk) => {
+            sms += chunk.toString()
+        })
+
+        req.on('end', () => {
+            res.writeHead(200, { "Content-type": "application/json" })
+            res.end(sms)
+        })
+    } else if (method === "PUT") { 
+        let body = ""
+
+        req.on('data', (chunk) => { 
+            body += chunk.toString()
+
         })
 
         req.on('end', () => { 
-            res.writeHead('200', { "Content-type": "application/json" })
-            res.end(sms)
+            let parsedBody
+
+            try { 
+                parsedBody = JSON.parse(body)
+
+            }catch(err) { 
+                res.writeHead(400, { 'Content-type': 'application/json' })
+                res.end(JSON.stringify({ error: 'Invalid JSON format' }));
+                return;
+
+            }
+
+            const { title, description } = parsedBody;
+            const newCourse = {
+                id: crypto.randomUUID(),
+                title: title.trim(),
+                description: description.trim()
+            };
+            courses.push(newCourse);
+
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(newCourse));
         })
+        return 
     }
 })
 
